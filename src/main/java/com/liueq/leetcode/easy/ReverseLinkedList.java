d88eb363e617a2bdca6afa4805606a75f3d3a1e0
liueq <liueqmaaya@gmail.com>
2017-12-18 10:55:50 +0800

    ReverseLinkedList. 反转单向链表。

---
+++ src/main/java/com/liueq/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,45 @@
+package com.liueq.leetcode.easy;
+
+/**
+ * 问题描述：反转单向链表
+ *
+ * 解：
+ * 1 循环，从链表头插入达到反转的效果
+ * 2 递归，关键是对于中间状态的定一个 head.next.next = head, head.next = null
+ */
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseListRecursive(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode p = reverseListRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode fakeHead = new ListNode(-1);
+        fakeHead.next = head;
+        ListNode node = head;
+        while (node.next != null) {
+            ListNode p = node.next;
+            node.next = node.next.next;
+            p.next = fakeHead.next;
+            fakeHead.next = p;
+        }
+
+        return fakeHead.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
